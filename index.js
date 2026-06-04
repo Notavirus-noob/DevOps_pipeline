964afdba888920eccfb107976c921fd7db33ffb9
Sujin <sujin09818610719@gmail.com>
2026-06-04 13:19:36 +0545

    feat: add CI Pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log('Hello, DevOps!');
+console.log("I'm learning how to deploy applications with DevOps practices.");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log('Starting Test Script');
+setTimeout(() => {
+    console.log('Waiting for 2 seconds...');
+},2000);
+console.log('Test Completed');
